5c42046101907c9b366734d0d04314e731e473c5
englishwithjeffreyh-byte <englishwithjeffreyh@gmail.com>
2026-07-30 23:25:58 -0400

    Update archivo_service.py

--- python/services/archivo_service.py
+++ python/services/archivo_service.py
@@ -1,1 +1,18 @@
+import os
 
+
+class ArchivoService:
+
+    @staticmethod
+    def guardar_linea(nombre_archivo, contenido):
+        with open(nombre_archivo, "a", encoding="utf-8") as archivo:
+            archivo.write(contenido + "\n")
+
+    @staticmethod
+    def leer_archivo(nombre_archivo):
+
+        if not os.path.exists(nombre_archivo):
+            return []
+
+        with open(nombre_archivo, "r", encoding="utf-8") as archivo:
+            return archivo.readlines()
