ecbead0352c7f43ec276535c9ab57b9b27f2e980
englishwithjeffreyh-byte <englishwithjeffreyh@gmail.com>
2026-07-30 23:17:34 -0400

    Update curso.py

--- python/models/curso.py
+++ python/models/curso.py
@@ -1,1 +1,20 @@
+from .programa_academico import ProgramaAcademico
+
+class Curso(ProgramaAcademico):
+
+    def __init__(self):
+        super().__init__("Curso")
+
+    def evaluar_aprobacion(self, notas):
+        promedio = sum(notas) / len(notas)
+        return promedio >= 10
+
+
+
+
+
+
+
+
+
 
