7b881f31cd11f0c10d53ddec37fbc7e7ddb68651
englishwithjeffreyh-byte <englishwithjeffreyh@gmail.com>
2026-07-30 23:21:17 -0400

    Update profesor.py

--- python/models/profesor.py
+++ python/models/profesor.py
@@ -1,1 +1,19 @@
+from .persona import Persona
 
+
+class Profesor(Persona):
+
+    def __init__(self, cedula, nombre_completo, correo,
+                 especialidad, materia_asignada):
+
+        super().__init__(cedula, nombre_completo, correo)
+
+        self.especialidad = especialidad
+        self.materia_asignada = materia_asignada
+
+    def mostrar_informacion(self):
+        return (
+            f"{self.mostrar_datos()}\n"
+            f"Especialidad: {self.especialidad}\n"
+            f"Materia: {self.materia_asignada}"
+        )
