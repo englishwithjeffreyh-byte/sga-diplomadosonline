7eb00d463fdfcae98e9cae0124f9effcaad1ac84
englishwithjeffreyh-byte <englishwithjeffreyh@gmail.com>
2026-07-30 23:16:20 -0400

    Update programa_academico.py

--- python/models/programa_academico.py
+++ python/models/programa_academico.py
@@ -1,1 +1,10 @@
+from abc import ABC, abstractmethod
 
+class ProgramaAcademico(ABC):
+
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    @abstractmethod
+    def evaluar_aprobacion(self, notas):
+        pass
