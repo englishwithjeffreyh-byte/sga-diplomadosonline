ff61c382933ca2649cb9efc9f8e46fefaf9e84e5
englishwithjeffreyh-byte <englishwithjeffreyh@gmail.com>
2026-07-30 23:26:24 -0400

    Update sistema_gestion.py

--- python/services/sistema_gestion.py
+++ python/services/sistema_gestion.py
@@ -1,1 +1,8 @@
+class SistemaGestionAcademica:
 
+    def __init__(self):
+        self.alumnos = []
+        self.profesores = []
+
+        self.pila_notas = []
+        self.cola_certificados = []
