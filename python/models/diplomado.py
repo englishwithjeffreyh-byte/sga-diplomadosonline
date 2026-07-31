b766efa9299565329d042c7a0283e20e94a385c8
englishwithjeffreyh-byte <englishwithjeffreyh@gmail.com>
2026-07-30 23:18:10 -0400

    Update diplomado.py

--- python/models/diplomado.py
+++ python/models/diplomado.py
@@ -1,1 +1,21 @@
+from .programa_academico import ProgramaAcademico
+
+class Diplomado(ProgramaAcademico):
+
+    def __init__(self):
+        super().__init__("Diplomado")
+
+    def evaluar_aprobacion(self, notas):
+        promedio = sum(notas) / len(notas)
+        return promedio >= 14
+
+
+
+
+
+
+
+
+
+
 
