98bb79c788670f578d5c46cb68f63fbef82dccd1
englishwithjeffreyh-byte <englishwithjeffreyh@gmail.com>
2026-07-30 23:18:30 -0400

    Update bootcamp.py

--- python/models/bootcamp.py
+++ python/models/bootcamp.py
@@ -1,1 +1,9 @@
+from .programa_academico import ProgramaAcademico
 
+class Bootcamp(ProgramaAcademico):
+
+    def __init__(self):
+        super().__init__("Bootcamp")
+
+    def evaluar_aprobacion(self, notas):
+        return all(nota >= 14 for nota in notas)
