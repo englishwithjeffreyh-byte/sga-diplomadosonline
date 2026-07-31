f20de5b9ba29062c534824deff711eed73226154
englishwithjeffreyh-byte <englishwithjeffreyh@gmail.com>
2026-07-30 23:13:34 -0400

    Update persona.py

--- python/models/persona.py
+++ python/models/persona.py
@@ -1,1 +1,17 @@
+class Persona:
+    def __init__(self, cedula, nombre_completo, correo):
+        self.cedula = cedula
+        self.nombre_completo = nombre_completo
+        self.correo = correo
+
+    def mostrar_datos(self):
+        return (
+            f"Cédula: {self.cedula}\n"
+            f"Nombre: {self.nombre_completo}\n"
+            f"Correo: {self.correo}"
+        )
+
+
+
+
 
