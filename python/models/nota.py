10ffc0fe4457bae75d948072dd69747f1a14cf9c
englishwithjeffreyh-byte <englishwithjeffreyh@gmail.com>
2026-07-30 23:22:06 -0400

    Update nota.py

--- python/models/nota.py
+++ python/models/nota.py
@@ -1,1 +1,8 @@
+class Nota:
 
+    def __init__(self, cedula_alumno, valor):
+        self.cedula_alumno = cedula_alumno
+        self.valor = valor
+
+    def __str__(self):
+        return f"{self.cedula_alumno}: {self.valor}"
